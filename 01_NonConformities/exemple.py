4cc699207b916b6bd8744f5cea49af4e5b914436
Guilherme Tagliati <guilhermetagliati@outlook.com>
2022-11-14 08:49:57 -0300

    feat: Register Screen

--- 01_NonConformities/exemple.py
+++ 01_NonConformities/exemple.py
@@ -1,69 +1,192 @@
 import tkinter
 import tkinter.messagebox
 import customtkinter
-from PIL import Image, ImageTk
-import os
 
-customtkinter.set_appearance_mode("Dark")  # Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
-
-PATH = os.path.dirname(os.path.realpath(__file__))
 
 
 class App(customtkinter.CTk):
 
-    APP_NAME = "CustomTkinter example_background_image.py"
-    WIDTH = 900
-    HEIGHT = 600
+    WIDTH = 780
+    HEIGHT = 520
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self):
+        super().__init__()
 
-        self.title(App.APP_NAME)
+        self.title("CustomTkinter complex_example.py")
         self.geometry(f"{App.WIDTH}x{App.HEIGHT}")
-        self.minsize(App.WIDTH, App.HEIGHT)
-        self.maxsize(App.WIDTH, App.HEIGHT)
-        self.resizable(False, False)
+        self.protocol("WM_DELETE_WINDOW", self.on_closing)  # call .on_closing() when app gets closed
 
-        self.protocol("WM_DELETE_WINDOW", self.on_closing)
+        # ============ create two frames ============
 
-        # load image with PIL and convert to PhotoImage
-        image = Image.open(PATH + "/test_images/bg_gradient.jpg").resize((self.WIDTH, self.HEIGHT))
-        self.bg_image = ImageTk.PhotoImage(image)
+        # configure grid layout (2x1)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_rowconfigure(0, weight=1)
 
-        self.image_label = tkinter.Label(master=self, image=self.bg_image)
-        self.image_label.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+        self.frame_left = customtkinter.CTkFrame(master=self,
+                                                 width=180,
+                                                 corner_radius=0)
+        self.frame_left.grid(row=0, column=0, sticky="nswe")
 
-        self.frame = customtkinter.CTkFrame(master=self,
-                                            width=300,
-                                            height=App.HEIGHT,
-                                            corner_radius=0)
-        self.frame.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+        self.frame_right = customtkinter.CTkFrame(master=self)
+        self.frame_right.grid(row=0, column=1, sticky="nswe", padx=20, pady=20)
 
-        self.label_1 = customtkinter.CTkLabel(master=self.frame, width=200, height=60,
-                                              fg_color=("gray70", "gray25"), text="CustomTkinter\ninterface example")
-        self.label_1.place(relx=0.5, rely=0.3, anchor=tkinter.CENTER)
+        # ============ frame_left ============
 
-        self.entry_1 = customtkinter.CTkEntry(master=self.frame, corner_radius=6, width=200, placeholder_text="username")
-        self.entry_1.place(relx=0.5, rely=0.52, anchor=tkinter.CENTER)
+        # configure grid layout (1x11)
+        self.frame_left.grid_rowconfigure(0, minsize=10)   # empty row with minsize as spacing
+        self.frame_left.grid_rowconfigure(5, weight=1)  # empty row as spacing
+        self.frame_left.grid_rowconfigure(8, minsize=20)    # empty row with minsize as spacing
+        self.frame_left.grid_rowconfigure(11, minsize=10)  # empty row with minsize as spacing
 
-        self.entry_2 = customtkinter.CTkEntry(master=self.frame, corner_radius=6, width=200, show="*", placeholder_text="password")
-        self.entry_2.place(relx=0.5, rely=0.6, anchor=tkinter.CENTER)
+        self.label_1 = customtkinter.CTkLabel(master=self.frame_left,
+                                              text="CustomTkinter",
+                                              text_font=("Roboto Medium", -16))  # font name and size in px
+        self.label_1.grid(row=1, column=0, pady=10, padx=10)
 
-        self.button_2 = customtkinter.CTkButton(master=self.frame, text="Login",
-                                                corner_radius=6, command=self.button_event, width=200)
-        self.button_2.place(relx=0.5, rely=0.7, anchor=tkinter.CENTER)
+        self.button_1 = customtkinter.CTkButton(master=self.frame_left,
+                                                text="CTkButton",
+                                                command=self.button_event)
+        self.button_1.grid(row=2, column=0, pady=10, padx=20)
+
+        self.button_2 = customtkinter.CTkButton(master=self.frame_left,
+                                                text="CTkButton",
+                                                command=self.button_event)
+        self.button_2.grid(row=3, column=0, pady=10, padx=20)
+
+        self.button_3 = customtkinter.CTkButton(master=self.frame_left,
+                                                text="CTkButton",
+                                                command=self.button_event)
+        self.button_3.grid(row=4, column=0, pady=10, padx=20)
+
+        self.label_mode = customtkinter.CTkLabel(master=self.frame_left, text="Appearance Mode:")
+        self.label_mode.grid(row=9, column=0, pady=0, padx=20, sticky="w")
+
+        self.optionmenu_1 = customtkinter.CTkOptionMenu(master=self.frame_left,
+                                                        values=["Light", "Dark", "System"],
+                                                        command=self.change_appearance_mode)
+        self.optionmenu_1.grid(row=10, column=0, pady=10, padx=20, sticky="w")
+
+        # ============ frame_right ============
+
+        # configure grid layout (3x7)
+        self.frame_right.rowconfigure((0, 1, 2, 3), weight=1)
+        self.frame_right.rowconfigure(7, weight=10)
+        self.frame_right.columnconfigure((0, 1), weight=1)
+        self.frame_right.columnconfigure(2, weight=0)
+
+        self.frame_info = customtkinter.CTkFrame(master=self.frame_right)
+        self.frame_info.grid(row=0, column=0, columnspan=2, rowspan=4, pady=20, padx=20, sticky="nsew")
+
+        # ============ frame_info ============
+
+        # configure grid layout (1x1)
+        self.frame_info.rowconfigure(0, weight=1)
+        self.frame_info.columnconfigure(0, weight=1)
+
+        self.label_info_1 = customtkinter.CTkLabel(master=self.frame_info,
+                                                   text="CTkLabel: Lorem ipsum dolor sit,\n" +
+                                                        "amet consetetur sadipscing elitr,\n" +
+                                                        "sed diam nonumy eirmod tempor" ,
+                                                   height=100,
+                                                   corner_radius=6,  # <- custom corner radius
+                                                   fg_color=("white", "gray38"),  # <- custom tuple-color
+                                                   justify=tkinter.LEFT)
+        self.label_info_1.grid(column=0, row=0, sticky="nwe", padx=15, pady=15)
+
+        self.progressbar = customtkinter.CTkProgressBar(master=self.frame_info)
+        self.progressbar.grid(row=1, column=0, sticky="ew", padx=15, pady=15)
+
+        # ============ frame_right ============
+
+        self.radio_var = tkinter.IntVar(value=0)
+
+        self.label_radio_group = customtkinter.CTkLabel(master=self.frame_right,
+                                                        text="CTkRadioButton Group:")
+        self.label_radio_group.grid(row=0, column=2, columnspan=1, pady=20, padx=10, sticky="")
+
+        self.radio_button_1 = customtkinter.CTkRadioButton(master=self.frame_right,
+                                                           variable=self.radio_var,
+                                                           value=0)
+        self.radio_button_1.grid(row=1, column=2, pady=10, padx=20, sticky="n")
+
+        self.radio_button_2 = customtkinter.CTkRadioButton(master=self.frame_right,
+                                                           variable=self.radio_var,
+                                                           value=1)
+        self.radio_button_2.grid(row=2, column=2, pady=10, padx=20, sticky="n")
+
+        self.radio_button_3 = customtkinter.CTkRadioButton(master=self.frame_right,
+                                                           variable=self.radio_var,
+                                                           value=2)
+        self.radio_button_3.grid(row=3, column=2, pady=10, padx=20, sticky="n")
+
+        self.slider_1 = customtkinter.CTkSlider(master=self.frame_right,
+                                                from_=0,
+                                                to=1,
+                                                number_of_steps=3,
+                                                command=self.progressbar.set)
+        self.slider_1.grid(row=4, column=0, columnspan=2, pady=10, padx=20, sticky="we")
+
+        self.slider_2 = customtkinter.CTkSlider(master=self.frame_right,
+                                                command=self.progressbar.set)
+        self.slider_2.grid(row=5, column=0, columnspan=2, pady=10, padx=20, sticky="we")
+
+        self.switch_1 = customtkinter.CTkSwitch(master=self.frame_right,
+                                                text="CTkSwitch")
+        self.switch_1.grid(row=4, column=2, columnspan=1, pady=10, padx=20, sticky="we")
+
+        self.switch_2 = customtkinter.CTkSwitch(master=self.frame_right,
+                                                text="CTkSwitch")
+        self.switch_2.grid(row=5, column=2, columnspan=1, pady=10, padx=20, sticky="we")
+
+        self.combobox_1 = customtkinter.CTkComboBox(master=self.frame_right,
+                                                    values=["Value 1", "Value 2"])
+        self.combobox_1.grid(row=6, column=2, columnspan=1, pady=10, padx=20, sticky="we")
+
+        self.check_box_1 = customtkinter.CTkCheckBox(master=self.frame_right,
+                                                     text="CTkCheckBox")
+        self.check_box_1.grid(row=6, column=0, pady=10, padx=20, sticky="w")
+
+        self.check_box_2 = customtkinter.CTkCheckBox(master=self.frame_right,
+                                                     text="CTkCheckBox")
+        self.check_box_2.grid(row=6, column=1, pady=10, padx=20, sticky="w")
+
+        self.entry = customtkinter.CTkEntry(master=self.frame_right,
+                                            width=120,
+                                            placeholder_text="CTkEntry")
+        self.entry.grid(row=8, column=0, columnspan=2, pady=20, padx=20, sticky="we")
+
+        self.button_5 = customtkinter.CTkButton(master=self.frame_right,
+                                                text="CTkButton",
+                                                border_width=2,  # <- custom border_width
+                                                fg_color=None,  # <- no fg_color
+                                                command=self.button_event)
+        self.button_5.grid(row=8, column=2, columnspan=1, pady=20, padx=20, sticky="we")
+
+        # set default values
+        self.optionmenu_1.set("Dark")
+        self.button_3.configure(state="disabled", text="Disabled CTkButton")
+        self.combobox_1.set("CTkCombobox")
+        self.radio_button_1.select()
+        self.slider_1.set(0.2)
+        self.slider_2.set(0.7)
+        self.progressbar.set(0.5)
+        self.switch_2.select()
+        self.radio_button_3.configure(state=tkinter.DISABLED)
+        self.check_box_1.configure(state=tkinter.DISABLED, text="CheckBox disabled")
+        self.check_box_2.select()
 
     def button_event(self):
-        print("Login pressed - username:", self.entry_1.get(), "password:", self.entry_2.get())
+        print("Button pressed")
+
+    def change_appearance_mode(self, new_appearance_mode):
+        customtkinter.set_appearance_mode(new_appearance_mode)
 
     def on_closing(self, event=0):
         self.destroy()
 
-    def start(self):
-        self.mainloop()
-
 
 if __name__ == "__main__":
     app = App()
-    app.start()
+    app.mainloop()
--- 01_NonConformities/main.py
+++ 01_NonConformities/main.py
@@ -1,61 +1,143 @@
-import customtkinter
 import tkinter
+import tkinter.messagebox
+import customtkinter
 from PIL import Image, ImageTk
 import os
-
-# region Initial Setup
-PATH = os.path.dirname(os.path.realpath(__file__))  # Current File
 
 # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_appearance_mode("System")
 # Themes: "blue" (standard), "green", "dark-blue"
 customtkinter.set_default_color_theme("blue")
 
-# endregion
+PATH = os.path.dirname(os.path.realpath(__file__))  # Current Work Directory
 
 
 class App(customtkinter.CTk):
-    # region Properties
+
     CONTEXT = 16
     WIDTH = 1280
     HEIGHT = 720
-    # endregion
 
     def __init__(self):
         super().__init__()
-        self.geometry("1280x720")  # Small Display
-        self.title("Brains DP - Sistema de Abertura de Nao Conformidades")
-        self.grid_rowconfigure(0, weight=self.CONTEXT, minsize=self.CONTEXT)
-        self.grid_columnconfigure(0, weight=self.CONTEXT, minsize=self.CONTEXT)
 
-        image = Image.open(
-            PATH + "/assets/background.jpg").resize((self.WIDTH, self.HEIGHT))
-        self.img_bg = ImageTk.PhotoImage(image)
-        self.tkl_bg = tkinter.Label(master=self, image=self.img_bg)
-        self.tkl_bg.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+        self.title("CustomTkinter complex_example.py")
+        self.geometry(f"{App.WIDTH}x{App.HEIGHT}")
+        # call .on_closing() when app gets closed
+        self.protocol("WM_DELETE_WINDOW", self.on_closing)
 
-        self.ctkf_body = customtkinter.CTkFrame(
-            master=self, corner_radius=15, fg_color="gray18")
-        self.ctkf_body.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
-        self.ctkf_body.grid_columnconfigure(1, weight=1)
-        self.ctkf_body.grid_columnconfigure(1, weight=1)
+        # region LAYOUT CONFIGURATION
+        # configure grid layout (2x1)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_rowconfigure(0, weight=1)
 
-        self.img_logo = self.load_png_img("./assets/33.png", 280, 77)
-        self.ctkb_logo = customtkinter.CTkButton(
-            master=self.ctkf_body, image=self.img_logo, text="", fg_color="gray18", hover_color="gray18", corner_radius=10)
-        self.ctkb_logo.grid(row=0, column=0, columnspan=1,
-                            padx=20, pady=10, sticky="e")
+        self.ctkf_sidebar = customtkinter.CTkFrame(master=self,
+                                                   width=180,
+                                                   corner_radius=0)
+        self.ctkf_sidebar.grid(row=0, column=0, sticky="nswe",
+                               padx=App.CONTEXT, pady=App.CONTEXT)
 
-        self.ctkl_h1 = customtkinter.CTkLabel(
-            master=self.ctkf_body, fg_color="gray18", text="Sistema de nao Conformidades")
-        self.ctkl_h1.grid(row=0, column=0, columnspan=1,
-                          padx=20, pady=10, sticky="e")
+        self.ctkf_main = customtkinter.CTkFrame(master=self)
+        self.ctkf_main.grid(row=0, column=1, sticky="nswe",
+                            padx=App.CONTEXT, pady=App.CONTEXT)
+        # endregion
 
-    # region Utilities Functions
+        # region SIDEBAR CONFIGURATION
+        
+        self.ctkf_sidebar.grid_rowconfigure(0, minsize=10)
+        self.ctkf_sidebar.grid_rowconfigure(5, weight=1)
+        self.ctkf_sidebar.grid_rowconfigure(8, minsize=20)
+        self.ctkf_sidebar.grid_rowconfigure(11, minsize=10)
+
+        self.img_logo = self.load_png_img("./assets/33.png", 210, 57)
+        self.ctkb_logo = customtkinter.CTkButton(master=self.ctkf_sidebar, image=self.img_logo, text="", fg_color="gray18", hover_color="gray18", corner_radius=10)
+        self.ctkb_logo.grid(row=1, column=0, pady=App.CONTEXT, padx=App.CONTEXT)
+        self.ctkb_create_menu = customtkinter.CTkButton(master=self.ctkf_sidebar,
+                                                text="Registro",
+                                                command=self.button_event, fg_color="grey30")
+        self.ctkb_create_menu.grid(row=2, column=0, sticky="we")
+        # endregion
+
+        # region MAIN BODY CONFIGURATION
+        self.ctkf_main.grid_rowconfigure(0, minsize=10)
+        self.ctkf_main.grid_rowconfigure(1, weight=1)
+        self.ctkf_main.grid_columnconfigure(0, weight=1)
+
+        self.ctkl_page_title = customtkinter.CTkLabel(master=self.ctkf_main,
+                                              text="Sistema de Abertura de Nao Conformidades V2",
+                                              text_font=("Bebas Neue", 2*App.CONTEXT)) 
+        self.ctkl_page_title.grid(row=0, column=0, pady=App.CONTEXT, padx=App.CONTEXT)
+        
+        self.ctkf_form_content = customtkinter.CTkFrame(master=self.ctkf_main)
+        self.ctkf_form_content.grid(row=1, column=0, sticky="nswe",
+                            padx=App.CONTEXT, pady=App.CONTEXT)
+        
+        #endregion
+
+        #region FORM
+        self.ctkf_form_content.grid_columnconfigure(0, weight=1)
+        self.ctkf_form_content.grid_columnconfigure(1, weight=1)
+        self.ctkf_form_content.grid_rowconfigure(0, minsize=10)     # empty row with minsize as spacing
+
+
+        self.ctkl_identity = customtkinter.CTkLabel(master=self.ctkf_form_content,
+                                              text="Identificacao",
+                                              text_font=("Roboto Medium", -16)) 
+        self.ctkl_identity.grid(row=1, column=0,  sticky="w", pady=10, padx=0)
+
+        self.ctks_identity = customtkinter.CTkOptionMenu(master=self.ctkf_form_content,
+                                                         values=["Light", "Dark", "System"])
+        self.ctks_identity.grid(row=2, column=0,  sticky="we", pady=10, padx=20)
+        
+        self.ctkl_description = customtkinter.CTkLabel(master=self.ctkf_form_content,
+                                              text="Descricao",
+                                              text_font=("Roboto Medium", -16)) 
+        self.ctkl_description.grid(row=3, column=0,  sticky="w", pady=10, padx=0)
+        self.ctke_description = customtkinter.CTkTextbox(master=self.ctkf_form_content,
+                                            height=App.CONTEXT*5, fg_color="grey25")
+        self.ctke_description.insert("0.0","Escreva aqui a descricao da nao conformidade")
+        self.ctke_description.grid(row=4, column=0,  sticky="we", pady=10, padx=20)
+
+        self.ctkl_rootcause = customtkinter.CTkLabel(master=self.ctkf_form_content,
+                                              text="Analise de Causa Raiz",
+                                              text_font=("Roboto Medium", -16)) 
+        self.ctkl_rootcause.grid(row=3, column=1,  sticky="w", pady=10, padx=20)
+        self.ctke_rootcause = customtkinter.CTkTextbox(master=self.ctkf_form_content,
+                                            height=App.CONTEXT*5, fg_color="grey25")
+        self.ctke_rootcause.insert("0.0","Escreva aqui a sua analise de causa raiz")
+        self.ctke_rootcause.grid(row=4, column=1,  sticky="we", pady=10, padx=20)
+
+        self.ctkl_solution = customtkinter.CTkLabel(master=self.ctkf_form_content,
+                                              text="Acoes corretivas",
+                                              text_font=("Roboto Medium", -16)) 
+        self.ctkl_solution.grid(row=5, column=0,  sticky="w", pady=10, padx=20)
+
+        self.ctke_solution = customtkinter.CTkTextbox(master=self.ctkf_form_content,
+                                            height=App.CONTEXT*5, fg_color="grey25")
+        self.ctke_solution.insert("0.0","Escreva aqui as acoes tomadas para solucionar o problema")
+        self.ctke_solution.grid(row=6, column=0,  sticky="we", pady=10, padx=20)
+
+        self.ctkcheck_critical = customtkinter.CTkCheckBox(master=self.ctkf_form_content,
+                                                     text="Critico")
+        self.ctkcheck_critical.grid(row=6, column=1,  sticky="we", pady=10, padx=20)
+
+
+        self.ctkb_create = customtkinter.CTkButton(master=self.ctkf_form_content,
+                                                text="Criar",
+                                                command=self.button_event)
+        self.ctkb_create.grid(row=7, column=0, pady=10, padx=20, sticky="nsew")                           
+        #endregion
+
+
+    def button_event(self):
+        print("Button pressed")
+
+    def on_closing(self, event=0):
+        self.destroy()
+
     def load_png_img(self, path, img_width, img_height):
         """ Load PNG from path"""
         return ImageTk.PhotoImage(Image.open(PATH + path).resize((img_width, img_height)))
-    # endregion
 
 
 if __name__ == "__main__":
